ff96ba85d9a6573fc726e9d1b5c20f08bd2b049a
Carlos Henrique F Jorge <carlhenrique@gmail.com>
2017-11-28 16:40:20 -0200

    mudei de novo o projeto

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hello <strong>World</strong> - Primeiro código em PHP - código modificado no git";
-
+echo "Hello <strong>World</strong> - Primeiro código em PHP 123 - código modificado no git";
+echo "Olá Carlim";
 ?>
